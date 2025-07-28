f5541e6e23f27931db37d6a2696608da23b59ed3
reborndigitals <reborndigitals@gmail.com>
2025-07-28 20:01:35 +0530

    config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-API_ID = int(os.getenv("API_ID", 0))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("API_ID", "10284859"))
+API_HASH = os.getenv("API_HASH", "b0ad58eb8b845ba0003e0d9ce5fc2196")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8393938399:AAFeJosaqckHB3HCFBF5Ov1pZkZSLwehW6w")
